f058a7bcdd8f53138db4a87e11895c6e0e0ced0d
ed murphy <edzillion@gmail.com>
2018-05-11 23:31:01 +0200

    change jest mode to react-native

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './src/aws-exports';
+
+Amplify.configure(aws_exports);
+
 export default class App extends React.Component {
   render() {
     return (
