2f4fdc90049410c9930ca61229ed9dc033945cd2
ed murphy <edzillion@gmail.com>
2018-05-12 12:58:36 +0200

    standard.js restyling

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-import Amplify from 'aws-amplify';
-import aws_exports from './src/aws-exports';
+import Amplify from 'aws-amplify'
+import awsExports from './src/aws-exports'
 
-Amplify.configure(aws_exports);
+Amplify.configure(awsExports)
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -23,6 +23,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
