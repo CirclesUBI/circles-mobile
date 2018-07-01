20f610055e733619b2030d8a66e911299efd53d4
shkfnly <ashoka.finley@gmail.com>
2018-07-01 14:39:35 -0700

    Install React-Navigation

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import { Navigation } from 'react-native-navigation'
 
 import Amplify from 'aws-amplify'
 import awsExports from './aws-exports'
 
 import { withAuthenticator } from 'aws-amplify-react-native'
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
 Amplify.configure(awsExports)
 
-class App extends React.Component {
+class AppHome extends React.Component {
   render () {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <TouchableHighlight onPress={() => this.props.navigation.replace('Tabs')}>
+          <Text>Touch Me</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+}
+
+class App extends React.Component {
+  render () {
+    return (
+      <StartNavigator />
+    )
+  }
+}
+
+class TabHome extends React.Component {
+  render () {
+    return <TabNavigator />
+  }
+}
+class Home extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>HOME</Text>
+      </View>
+    )
+  }
+}
+class Transact extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>TRANSACT</Text>
+      </View>
+    )
+  }
+}
+class Search extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>SEARCH</Text>
       </View>
     )
   }
@@ -26,6 +73,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center'
   }
+})
+
+const StartNavigator = createStackNavigator({
+  Intro: {
+    screen: AppHome
+  },
+  Tabs: {
+    screen: TabHome
+  }}, {
+    headerMode: 'none'
+  })
+
+const TabNavigator = createBottomTabNavigator({
+  Home: Home,
+  Transact: Transact,
+  Search: Search
+}, {
+  headerMode: 'none'
 })
 
 // export default withAuthenticator(App)
