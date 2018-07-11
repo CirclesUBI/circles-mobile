3f19198d3894deaedf13c1daf2a14a0cdced37b0
Ashoka Finley <ashoka.finley@gmail.com>
2018-07-11 12:25:03 -0400

    Install component sets (#3)
    * installation works
    
    * Install React-Navigation
    
    * Installed React Navigation...Did initial wiring up
    
    * Implement AntD component library
    
    * Add routing for validate screen
    
    * Implemented React Navigation
    
    * Implement Custom Tab Bar
    
    * Add Pay/Request Screens
    
    * Add Org Wallet Setting Screen
    
    * Add Org Home Screen
    
    * Install React Redux

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
+// import Expo from 'expo'
 import Amplify from 'aws-amplify'
 import awsExports from './aws-exports'
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
 import { withAuthenticator } from 'aws-amplify-react-native'
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
@@ -9,18 +13,46 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import SplashScreen from './lib/components/SplashScreen'
 import ConnectScreen from './lib/components/ConnectScreen' // Add Container
 import WalletScreen from './lib/components/WalletScreen'
-import TransactionScreen from './lib/components/TransactionScreen'
+// import TransactionScreen from './lib/components/TransactionScreen'
 // import ConnectContainer from './lib/containers/ConnectContainer'
+
 import HomeScreen from './lib/components/HomeScreen'
+import ValidatePhone from './lib/components/Validate/ValidatePhone'
+import ValidateSuccess from './lib/components/Validate/ValidateSuccess'
 
-import addOrgWallet from './lib/components/AddOrgWallet/AddWallet'
+import addWallet from './lib/components/AddOrgWallet/AddWallet'
+import addOffer from './lib/components/AddOrgWallet/AddOffer'
+import addAdmin from './lib/components/AddOrgWallet/AddAdmin'
 
+import PayAmount from './lib/components/Pay/PayAmount'
+import RequestAmount from './lib/components/Request/RequestAmount'
+
+import OrgAddInventory from './lib/components/OrgWallet/OrgAddInventory'
+import OrgWalletScreen from './lib/components/OrgWallet/OrgWalletScreen'
+import OrgWalletSettings from './lib/components/OrgWallet/OrgWalletSettingsScreen'
+
+import Tabs from './lib/components/Tabs'
+import { MenuProvider } from 'react-native-popup-menu'
+
+const TxPlaceholder = () => (<View />)
+
+let store = createStore(combineReducers({users: (state = {}, action) => state}))
 Amplify.configure(awsExports)
 
 class App extends React.Component {
+  // async componentWillMount () {
+  //   await Expo.Font.loadAsync({
+  //     'Roboto': require('native-base/Fonts/Roboto.ttf'),
+  //     'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf')
+  //   })
+  // }
   render () {
     return (
-      <StartNavigator />
+      <Provider store={store}>
+        <MenuProvider>
+          <StartNavigator />
+        </MenuProvider>
+      </Provider>
     )
   }
 }
@@ -35,25 +67,35 @@ class Search extends React.Component {
   }
 }
 
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Transact: TransactionScreen,
-  Search: Search
+const ValidateNavigator = createStackNavigator({
+  ValidatePhone: {
+    screen: ValidatePhone
+  },
+  ValidateSuccess: {
+    screen: ValidateSuccess
+  }
 }, {
   headerMode: 'none'
 })
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+const HomeNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  WalletView: {
+    screen: WalletScreen
+  },
+  OrgWalletView: {
+    screen: OrgWalletScreen
+  },
+  Validate: {
+    screen: ValidateNavigator
+  }}, {
+    headerMode: 'none'
+  })
 
 const IntroNavigator = createStackNavigator({
-  Intro: {
+  Splash: {
     screen: SplashScreen
   },
   Connect: {
@@ -62,24 +104,72 @@ const IntroNavigator = createStackNavigator({
     headerMode: 'none'
   })
 
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeNavigator,
+  Transact: TxPlaceholder,
+  Search: Search
+}, {
+  headerMode: 'none',
+  tabBarComponent: Tabs
+})
+
+const OrgWalletNavigator = createStackNavigator({
+  'addOrgWallet.AddWallet': {
+    screen: addWallet
+  },
+  'addOrgWallet.AddOffer': {
+    screen: addOffer
+  },
+  'addOrgWallet.AddAdmin': {
+    screen: addAdmin
+  }}, {
+    headerMode: 'none'
+  })
+
+const RequestNavigator = createStackNavigator({
+  'RequestAmount': {
+    screen: RequestAmount
+  }
+}, {
+  headerMode: 'none'
+})
+
+const PayNavigator = createStackNavigator({
+  'PayAmount': {
+    screen: PayAmount
+  }
+}, {
+  headerMode: 'none'
+})
+
 const MainNavigator = createStackNavigator({
   Intro: {
     screen: IntroNavigator
   },
   Tabs: {
     screen: TabNavigator
+  },
+  'addOrgWallet': {
+    screen: OrgWalletNavigator
+  },
+  'OrgWalletSettings': {
+    screen: OrgWalletSettings
+  },
+  OrgInventory: {
+    screen: OrgAddInventory
+  },
+  'Pay': {
+    screen: PayNavigator
+  },
+  'Request': {
+    screen: RequestNavigator
   }}, {
     headerMode: 'none'
   })
+
 const StartNavigator = createStackNavigator({
   Main: {
     screen: MainNavigator
-  },
-  WalletView: {
-    screen: WalletScreen
-  },
-  'addOrgWallet.AddWallet': {
-    screen: addOrgWallet
   }}, {
     mode: 'modal',
     headerMode: 'none'
