97d82dc38a3462ba76f156fc659c450d24d0b865
shkfnly <ashoka.finley@gmail.com>
2018-08-10 10:55:30 +0200

    all standalone apps work

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "";
+  public static final String DEVELOPMENT_URL = "2odDf3tfWkap1kPqAHYm7ow2B1m2SisVK5M://aa-zbp.shokishoki.circles-mobile.exp.direct:80";
 
 }
