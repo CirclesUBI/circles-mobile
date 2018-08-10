6505bebec1bcfa54ec3ddb311c71c4edb248098e
shkfnly <ashoka.finley@gmail.com>
2018-08-10 08:28:43 +0200

    Uport Integration Finished

--- ios/circles-mobile/AppDelegate.m
+++ ios/circles-mobile/AppDelegate.m
@@ -65,11 +65,4 @@
     [[ExpoKit sharedInstance] application:application didRegisterUserNotificationSettings:notificationSettings];
 }
 
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-    return [RCTLinkingManager application:application openURL:url options:options];
-}
-
 @end
