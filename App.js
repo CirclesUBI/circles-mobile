87afc67ef872d79fe76035324075fd20604f8277
ed murphy <edzillion@gmail.com>
2018-11-21 21:01:35 +0100

    working s3 upload, added identity pool id to config

--- App.js
+++ App.js
@@ -9,36 +9,46 @@ import { StartNavigator } from 'circles-mobile/lib/navigators/RootNavigation'
 import Amplify from 'aws-amplify'
 // Amplify.Logger.LOG_LEVEL = 'DEBUG'
 
-import { AWS_REGION, USER_POOL_ID, USER_POOL_CLIENT_ID, API_TEST_ENDPOINT, S3_BUCKET } from 'react-native-dotenv'
+import { AWS_REGION, USER_POOL_ID, USER_POOL_CLIENT_ID, API_TEST_ENDPOINT, S3_BUCKET, IDENTITY_POOL_ID } from 'react-native-dotenv'
 
 Amplify.configure({
   Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: IDENTITY_POOL_ID,
 
     // REQUIRED - Amazon Cognito Region
     region: AWS_REGION,
 
+    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+    // Required only if it's different from Amazon Cognito Region
+    // identityPoolRegion: 'XX-XXXX-X',
+
     // OPTIONAL - Amazon Cognito User Pool ID
     userPoolId: USER_POOL_ID,
 
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
     userPoolWebClientId: USER_POOL_CLIENT_ID,
 
     // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-    mandatorySignIn: true
+    mandatorySignIn: false
 
     // // OPTIONAL - Configuration for cookie storage
     // cookieStorage: {
-    //   // REQUIRED - Cookie domain (only required if cookieStorage is provided)
-    //   domain: '.joincircles.net',
-    //   // OPTIONAL - Cookie path
-    //   path: '/',
-    //   // OPTIONAL - Cookie expiration in days
-    //   expires: 365,
-    //   // OPTIONAL - Cookie secure flag
-    //   secure: true
-    // }
+    // // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+    //     domain: '.yourdomain.com',
+    // // OPTIONAL - Cookie path
+    //     path: '/',
+    // // OPTIONAL - Cookie expiration in days
+    //     expires: 365,
+    // // OPTIONAL - Cookie secure flag
+    //     secure: true
+    // },
+
+    // OPTIONAL - customized storage object
+    // storage: new MyStorage(),
 
     // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-    // authenticationFlowType: AUTH_FLOW_TYPE
+    // authenticationFlowType: 'USER_PASSWORD_AUTH'
   },
   API: {
     endpoints: [
