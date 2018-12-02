4c695d76f7c6f6e9a09c5beb35cfee4714c09dfd
edzillion <edzillion@gmail.com>
2018-12-02 16:34:45 +0100

    Cognito auth (#29)
    * SMS verif & conf
    
    * broken cognito login process
    
    * amplify auth not firing sms
    
    * ditch sms verif for now, auto signin on splash
    
    * login skip onboarding
    
    * auth test working api
    
    * cleanup & move resource addresses into .env file
    
    * cleanup
    
    * updated packages & move root navigation
    
    * cleanup logging
    
    * cleanup console.logs etc
    
    * cleanup cotd.
    
    * cleanup cotd.
    
    * wip logout
    
    * signin singout working
    
    * updated travis build to expo cli
    
    * update expo pkg
    
    * increase max watchers on build script
    
    * change to process.env for build
    
    * add react-native-maps
    
    * added email input, pword still hardcoded
    
    * changed env vars for build
    
    * added guest login
    
    * env vars again
    
    * write env file on travis
    
    * test create-env
    
    * forgot to install create-env
    
    * expo pub test w alerts
    
    * hacky save both on continue
    
    * basic phone formatting/validating
    
    * rem alerts and readd API test
    
    * amplify storage
    
    * working s3 upload, added identity pool id to config
    
    * draft camera func
    
    * photo uploading
    
    * navigation reset issues
    
    * fixed reset stack on login
    
    * Onboarding Saga Example
    
    * basic photo resize and upload
    
    * signUp saga working
    
    * added verifyUser action to deal with verif state
    
    * wip AuthSaga
    
    * changing user pool
    
    * photo upload working via saga
    
    * auth saga
    
    * rem hardcoded creds and make picture mandatory

--- App.js
+++ App.js
@@ -2,49 +2,71 @@ import React from 'react'
 import { Font, AppLoading } from 'expo'
 import { Provider } from 'react-redux'
 
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
-
-import SplashScreen from 'circles-mobile/lib/components/SplashScreen'
-import TermsConditionsScreen from 'circles-mobile/lib/components/Onboarding/OnboardingTerms'
-import AvatarScreen from 'circles-mobile/lib/components/Onboarding/OnboardingAvatar'
-import PhoneScreen from 'circles-mobile/lib/components/Onboarding/OnboardingPhone'
-import VerifyPhoneScreen from 'circles-mobile/lib/components/Onboarding/OnboardingVerifyPhone'
-import TestnetScreen from 'circles-mobile/lib/components/Onboarding/OnboardingTestnetWarning'
-import CompleteScreen from 'circles-mobile/lib/components/Onboarding/OnboardingComplete'
-
-import ConnectScreen from 'circles-mobile/lib/components/ConnectScreen' // Add Container
-import WalletScreen from 'circles-mobile/lib/components/WalletScreen'
-// import TransactionScreen from 'circles-mobile/lib/components/TransactionScreen'
-// import ConnectContainer from 'circles-mobile/lib/containers/ConnectContainer'
-
-import HomeDrawerNavigator from 'circles-mobile/lib/navigators/HomeNavigator'
-import ValidatePhone from 'circles-mobile/lib/components/Validate/ValidatePhone'
-import ValidateSuccess from 'circles-mobile/lib/components/Validate/ValidateSuccess'
-
-import addWallet from 'circles-mobile/lib/components/AddOrgWallet/AddWallet'
-import addOffer from 'circles-mobile/lib/components/AddOrgWallet/AddOffer'
-import addAdmin from 'circles-mobile/lib/components/AddOrgWallet/AddAdmin'
-
-import PayAmount from 'circles-mobile/lib/components/Pay/PayAmount'
-import PayConfirm from 'circles-mobile/lib/components/Pay/PayConfirm'
-
-import RequestAmount from 'circles-mobile/lib/components/Request/RequestAmount'
-import RequestQR from 'circles-mobile/lib/components/Request/RequestQR'
-import RequestConfirm from 'circles-mobile/lib/components/Request/RequestConfirm'
-
-import OrgAddInventory from 'circles-mobile/lib/components/OrgWallet/OrgAddInventory'
-import OrgWalletScreen from 'circles-mobile/lib/components/OrgWallet/OrgWalletScreen'
-import OrgWalletSettings from 'circles-mobile/lib/components/OrgWallet/OrgWalletSettingsScreen'
-
-import OrgHomeDrawerNavigator from 'circles-mobile/lib/navigators/OrgHomeNavigator'
-import Contacts from 'circles-mobile/lib/components/Contacts'
-
-import SearchScreen from 'circles-mobile/lib/containers/SearchContainer'
-import Scanner from 'circles-mobile/lib/components/Scanner/Scanner'
-
-import Tabs from 'circles-mobile/lib/components/Tabs'
 import { MenuProvider } from 'react-native-popup-menu'
 import store from 'circles-mobile/lib/store'
+import { StartNavigator } from 'circles-mobile/lib/navigators/RootNavigation'
+import NavigationService from 'circles-mobile/lib/navigators/NavigationService'
+
+import Amplify from 'aws-amplify'
+// Amplify.Logger.LOG_LEVEL = 'DEBUG'
+
+import { AWS_REGION, USER_POOL_ID, USER_POOL_CLIENT_ID, API_TEST_ENDPOINT, S3_BUCKET, IDENTITY_POOL_ID } from 'react-native-dotenv'
+
+global.self = global
+global.Buffer = global.Buffer || require('buffer').Buffer
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: IDENTITY_POOL_ID,
+
+    // REQUIRED - Amazon Cognito Region
+    region: AWS_REGION,
+
+    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+    // Required only if it's different from Amazon Cognito Region
+    // identityPoolRegion: 'XX-XXXX-X',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: USER_POOL_ID,
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: USER_POOL_CLIENT_ID,
+
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: false
+
+    // // OPTIONAL - Configuration for cookie storage
+    // cookieStorage: {
+    // // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+    //     domain: '.yourdomain.com',
+    // // OPTIONAL - Cookie path
+    //     path: '/',
+    // // OPTIONAL - Cookie expiration in days
+    //     expires: 365,
+    // // OPTIONAL - Cookie secure flag
+    //     secure: true
+    // },
+
+    // OPTIONAL - customized storage object
+    // storage: new MyStorage(),
+
+    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+    // authenticationFlowType: 'USER_PASSWORD_AUTH'
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'circles',
+        endpoint: API_TEST_ENDPOINT
+      }
+    ]
+  },
+  Storage: {
+    bucket: S3_BUCKET,
+    region: AWS_REGION
+  }
+})
 
 class App extends React.Component {
   constructor () {
@@ -62,176 +84,17 @@ class App extends React.Component {
     })
     this.setState({loading: false})
   }
+
   render () {
     // <Provider store={store}>
     return this.state.loading
       ? <AppLoading />
       : (<Provider store={store}>
         <MenuProvider>
-          <StartNavigator />
+          <StartNavigator ref={navigatorRef => NavigationService.setTopLevelNavigator(navigatorRef)} />
         </MenuProvider>
       </Provider>)
   }
 }
 
-const ValidateNavigator = createStackNavigator({
-  ValidatePhone: {
-    screen: ValidatePhone
-  },
-  ValidateSuccess: {
-    screen: ValidateSuccess
-  }
-}, {
-  headerMode: 'none'
-})
-
-const HomeNavigator = createStackNavigator({
-  HomeScreen: {
-    screen: HomeDrawerNavigator
-  },
-  WalletView: {
-    screen: WalletScreen
-  },
-  OrgWalletView: {
-    screen: OrgWalletScreen
-  },
-  Validate: {
-    screen: ValidateNavigator
-  }}, {
-  headerMode: 'none'
-})
-
-const OrgHomeNavigator = createStackNavigator({
-  OrgHome: {
-    screen: OrgHomeDrawerNavigator
-  },
-  OrgWalletView: {
-    screen: OrgWalletScreen
-  }}, {
-  headerMode: 'none'
-})
-
-const IntroNavigator = createStackNavigator({
-  Splash: {
-    screen: SplashScreen
-  },
-  Terms: {
-    screen: TermsConditionsScreen
-  },
-  Avatar: {
-    screen: AvatarScreen
-  },
-  Phone: {
-    screen: PhoneScreen
-  },
-  VerifyPhone: {
-    screen: VerifyPhoneScreen
-  },
-  Testnet: {
-    screen: TestnetScreen
-  },
-  Complete: {
-    screen: CompleteScreen
-  },
-  Connect: {
-    screen: ConnectScreen
-  }
-}, {
-  headerMode: 'none'
-})
-
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeNavigator,
-  OrgHomeView: OrgHomeNavigator,
-  Search: SearchScreen
-}, {
-  headerMode: 'none',
-  tabBarComponent: Tabs
-})
-
-const OrgWalletNavigator = createStackNavigator({
-  'addOrgWallet.AddWallet': {
-    screen: addWallet
-  },
-  'addOrgWallet.AddOffer': {
-    screen: addOffer
-  },
-  'addOrgWallet.AddAdmin': {
-    screen: addAdmin
-  }}, {
-  headerMode: 'none'
-})
-
-const RequestNavigator = createStackNavigator({
-  'RequestAmount': {
-    screen: RequestAmount
-  },
-  'RequestQR': {
-    screen: RequestQR
-  },
-  'RequestContacts': {
-    screen: Contacts
-  },
-  'RequestConfirm': {
-    screen: RequestConfirm
-  }}, {
-  headerMode: 'none'
-})
-
-const PayNavigator = createStackNavigator({
-  'PayAmount': {
-    screen: PayAmount
-  },
-  'PayContacts': {
-    screen: Contacts
-  },
-  'PayConfirm': {
-    screen: PayConfirm
-  }}, {
-  headerMode: 'none'
-})
-
-const MainNavigator = createStackNavigator({
-  Intro: {
-    screen: IntroNavigator
-  },
-  Tabs: {
-    screen: TabNavigator
-  },
-  'addOrgWallet': {
-    screen: OrgWalletNavigator
-  },
-  'OrgWalletSettings': {
-    screen: OrgWalletSettings
-  },
-  OrgInventory: {
-    screen: OrgAddInventory
-  },
-  'Pay': {
-    screen: PayNavigator
-  },
-  'Request': {
-    screen: RequestNavigator
-  },
-  'Contacts': {
-    screen: Contacts
-  },
-  'Scanner': {
-    screen: Scanner
-  }}, {
-  headerMode: 'none'
-})
-
-const StartNavigator = createStackNavigator({
-  Main: {
-    screen: MainNavigator
-  },
-  'Scanner': {
-    screen: Scanner
-  }}, {
-  mode: 'modal',
-  headerMode: 'none'
-})
-
-// export default withAuthenticator(App)
 export default App
---
+++ todo.md
@@ -0,0 +1,16 @@
+[ ] Should I have auth in the form of a reducer? Login attrib on the user
+[ ] Auth status (loggedin/loggedout) as a saga?
+[ ] Unfinished TextInput for phone number prefixes etc
+[ ] Navigation, how to put hompage at start of navigation stack?
+[ ] Extra login screen? How do deal with passwords?
+[ ] Why isn't ant design css working?
+[ ] File placement? Singleton services in RN
+[x] How to deal with dotenv files on CI? 
+
+refactor validateUser -> verifyUser to future proof against future usage of validation terminology for our validator system
+
+location of VERIFY_USER? onboarding or user actions?
+
+need a flag for having finished onboarding, is address a good candidate?
+
+MFA questions: any use in adding TOTP? SMS verif for phones
