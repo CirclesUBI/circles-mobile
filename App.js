4d06c21bf55a590c79b7acaa9d2e578a9b175ee7
shkfnly <ashoka.finley@gmail.com>
2019-03-16 01:38:53 +0900

    (bug) variable name

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import LoadingSpinner from 'circles-mobile/lib/components/LoadingSpinner'
 
 import Amplify from 'aws-amplify'
 
-import { AWS_REGION, USER_POOL_ID, USER_POOL_CLIENT_ID, API_USER_ENDPOINT, API_RELAYER_ENDPOINT, S3_BUCKET, IDENTITY_POOL_ID } from 'react-native-dotenv'
+import { AWS_REGION, USER_POOL_ID, USER_POOL_CLIENT_ID, API_USER_ENDPOINT, API_RELAYER_ENDPOINT, API_ORGS_ENDPOINT, S3_BUCKET, IDENTITY_POOL_ID } from 'react-native-dotenv'
 
 const logger = new Amplify.Logger('App')
 
