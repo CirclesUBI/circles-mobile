42abbfbdcd20d924c4404d54f792cc0749aaf1ea
edzillion <edzillion@gmail.com>
2019-07-17 15:39:58 +0200

    bump ver num

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,6 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp3f70ce72f5d4437ab28a24abccc6c38d://192.168.178.27:19000";
+  public static final String DEVELOPMENT_URL = "exp3f70ce72f5d4437ab28a24abccc6c38d://127.0.0.1:19000";
 
 }
